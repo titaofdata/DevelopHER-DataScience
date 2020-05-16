733bf0a9aac83ac096d915e1dd7476e2ea0020a8
Shanelle Recheta <50011393+IRC-sgrace@users.noreply.github.com>
2020-05-16 11:46:33 -0600

    removed newline characters in headless server

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
-mkdir -p ~/streamlit/
+mkdir -p ~/.streamlit/
 
 echo "\
 [generate]\n\
 email = \"shanelle.grace.recheta@eee.upd.edu.ph\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+enableCORS = false
+port = $PORT
 " > ~/.streamlit/config.toml
